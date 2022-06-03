d02853ae184888f014c978252a7374566a080c42
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-03 12:20:09 +0100

    Add calculator challenge

--- src/main/java/com/zinkworks/Main.java
+++ src/main/java/com/zinkworks/Main.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("The total sum is " + ReadingUserInputChallenge.readNums());
+        //System.out.println("The total sum is " + ReadingUserInputChallenge.readNums());
+
+        // Simple calculator challenge
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println(calculator.getFirstNumber());
+        System.out.println(calculator.getSecondNumber());
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
+
     }
 }
